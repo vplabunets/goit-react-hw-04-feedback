70c4fdc3be2c060b2610e2a9a33a2ce3e08222a6
Volodymyr Labunets <vplabunets@gmail.com>
2022-10-13 00:23:41 +0300

    ex1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,9 @@
+import { FeedBack } from './feedback';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <FeedBack />
     </div>
   );
 };
